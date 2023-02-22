376e6605df837e55810f5015eca96b7d121ac830
Claudio Manoel <sh.claudio@gmail.com>
2023-02-22 01:56:01 -0300

    Desafio: Implementação da OrderRepositoryInterface e criação dos testes

--- src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -37,6 +37,7 @@ describe("Order repository test", () => {
   });
 
   it("should create a new order", async () => {
+
     const customerRepository = new CustomerRepository();
     const customer = new Customer("123", "Customer 1");
     const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
@@ -47,7 +48,7 @@ describe("Order repository test", () => {
     const product = new Product("123", "Product 1", 10);
     await productRepository.create(product);
 
-    const ordemItem = new OrderItem(
+    const orderItem = new OrderItem(
       "1",
       product.name,
       product.price,
@@ -55,7 +56,7 @@ describe("Order repository test", () => {
       2
     );
 
-    const order = new Order("123", "123", [ordemItem]);
+    const order = new Order("123", "123", [orderItem]);
 
     const orderRepository = new OrderRepository();
     await orderRepository.create(order);
@@ -71,14 +72,166 @@ describe("Order repository test", () => {
       total: order.total(),
       items: [
         {
-          id: ordemItem.id,
-          name: ordemItem.name,
-          price: ordemItem.price,
-          quantity: ordemItem.quantity,
+          id: orderItem.id,
+          name: orderItem.name,
+          // Para obter o preço unitário foi necessário dividir pela quantidade
+          // Lembrando que o get price do orderItem retorna o preço * quantidade
+          price: orderItem.price / orderItem.quantity,
+          quantity: orderItem.quantity,
           order_id: "123",
           product_id: "123",
         },
       ],
     });
   });
+
+
+  it("should update an order", async () => {
+    const customerRepository = new CustomerRepository();
+    
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const customer2 = new Customer("555", "Customer 2");
+    customer2.changeAddress(address);
+    await customerRepository.create(customer2);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order("123", "123", [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderItem2 = new OrderItem(
+      "12",
+      product.name,
+      product.price,
+      product.id,
+      5
+    );
+
+    const orderToUpdate = new Order("123", "555", [orderItem2]);
+    await orderRepository.update(orderToUpdate);
+    
+    const orderModel = await OrderModel.findOne({
+      where: { id: orderToUpdate.id },
+      include: ["items"],
+    });
+
+    expect(orderModel.toJSON()).toStrictEqual({
+      id: "123",
+      customer_id: "555",
+      total: orderToUpdate.total(),
+      items: [
+        {
+          id: orderItem2.id,
+          name: orderItem2.name,
+          price: orderItem2.price / orderItem2.quantity,
+          quantity: orderItem2.quantity,
+          order_id: "123",
+          product_id: "123",
+        },
+      ],
+    });
+
+  });
+
+
+  it("should find an order", async () => {
+    const customerRepository = new CustomerRepository();
+
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("742", "Product 2", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      3
+    );
+  
+    const order = new Order("145", "123", [orderItem]);
+  
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderResult = await orderRepository.find("145")
+  
+    expect(JSON.stringify(order)).toStrictEqual(JSON.stringify(orderResult));
+
+  });
+
+
+  it("should throw an error when order is not found", async () => {
+    const orderRepository = new OrderRepository();
+
+    expect(async () => {
+      await orderRepository.find("85SDSD");
+    }).rejects.toThrow("Order not found");
+  });
+
+  it("should find all customers", async () => {
+
+    const customerRepository = new CustomerRepository();
+    const productRepository = new ProductRepository();
+    const orderRepository = new OrderRepository();
+
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const orderItem2 = new OrderItem(
+      "11",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order1 = new Order("123", "123", [orderItem]);
+    const order2 = new Order("777", "123", [orderItem2]);
+
+    await orderRepository.create(order1);
+    await orderRepository.create(order2);
+
+    const orders = await orderRepository.findAll();
+
+    expect(orders).toHaveLength(2);
+    expect(orders).toContainEqual(order1);
+    expect(orders).toContainEqual(order2);
+  });
+
 });
+
--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
-export default class OrderRepository {
+export default class OrderRepository  implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -12,7 +14,9 @@ export default class OrderRepository {
         items: entity.items.map((item) => ({
           id: item.id,
           name: item.name,
-          price: item.price,
+          // Para obter o preço unitário foi necessário dividir pela quantidade
+          // Lembrando que o get price do orderItem retorna o preço * quantidade
+          price: item.price / item.quantity,
           product_id: item.productId,
           quantity: item.quantity,
         })),
@@ -21,5 +25,86 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+   async update(entity: Order): Promise<void> {
+
+    const filter = { where: { id: entity.id }, rejectOnEmpty: true, include: [{ model: OrderItemModel }] };
+    let order;
+
+    try{
+      order = await OrderModel.findOne(filter).then((model) => {
+        // Atualiza e salva os dados da ordem
+        model.customer_id = entity.customerId;
+        model.total = entity.total();
+        model.save();
+
+        // Apaga os itens existentes
+        model.items.map((item) => { item.destroy() });
+        
+        // Cria os novos itens baseados nos itens da entidade
+        entity.items.map((item) => {
+          OrderItemModel.create({
+            order_id: model.id,
+            id: item.id,
+            name: item.name,
+            price: item.price / item.quantity,
+            product_id: item.productId,
+            quantity: item.quantity,
+          })
+        })
+      })
+    } catch(error){
+      throw new Error("Order not found");
+    }
+  }
+  
+  async find(id: string): Promise<Order> {
+    const filter = { where: { id: id }, rejectOnEmpty: true, include: ['items']};
+    let orderModel;
+    
+    try {
+      orderModel = await OrderModel.findOne(filter);
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+
+    const result = new Order(
+      id, 
+      orderModel.customer_id,
+      orderModel.items.map((item) => new OrderItem(
+        item.id,
+        item.name,
+        item.price,
+        item.product_id,
+        item.quantity
+      ))
+    );
+
+    return result;
+  }
+
+  async findAll(): Promise<Order[]> {
+    let orderModelList;
+    
+    try {
+      orderModelList = await OrderModel.findAll({ include: [{ model: OrderItemModel }] });
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+
+    return orderModelList.map((orderModel) => 
+      new Order(
+        orderModel.id, 
+        orderModel.customer_id,
+        orderModel.items.map((item) => new OrderItem(
+          item.id,
+          item.name,
+          item.price,
+          item.product_id,
+          item.quantity
+        ))
+      )
+    )
   }
 }
